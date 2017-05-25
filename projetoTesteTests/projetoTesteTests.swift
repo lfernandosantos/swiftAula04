e71ad399b0e255e9087e25c4d7710763ceb10407
Luiz Dsantos <luiz.dsantos@sj208f11.local>
2017-05-25 19:22:22 -0300

    Initial Commit

---
+++ projetoTesteTests/projetoTesteTests.swift
@@ -0,0 +1,36 @@
+//
+//  projetoTesteTests.swift
+//  projetoTesteTests
+//
+//  Created by Luiz Dsantos on 25/05/17.
+//  Copyright © 2017 Luiz Dsantos. All rights reserved.
+//
+
+import XCTest
+@testable import projetoTeste
+
+class projetoTesteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
